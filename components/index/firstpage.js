7192d330d9b10d268e367684a47edcd9e89a93e6
muhammadmisbah <muhammadmisbah107@gmail.com>
2018-02-17 17:34:35 +0500

    integrate remember password

--- components/index/firstpage.js
+++ components/index/firstpage.js
@@ -7,11 +7,33 @@ import {
   StatusBar,
   Image
 } from 'react-native';
-import {Input, Item, Label, Button, Text, View} from 'native-base';
+import {Input, Item, Label, Button, Text, View, Spinner} from 'native-base';
 import {Actions} from 'react-native-router-flux';
 import styles from '../styles.js';
+import {connect} from 'react-redux';
+import * as firebase from 'firebase';
 
-export default class First extends Component{
+class First extends Component{
+  constructor(){
+    super();
+    this.state={
+      sign: false,
+    }
+  }
+  
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged((user)=> {
+      console.log(user);
+      if(user){
+        this.props.user(user);      
+        Actions.map();
+      }
+      else{
+        this.setState({sign: true})
+      }
+    })
+  }
+
   render() {
     return (
       <View style={styles.fcontainer}>
@@ -23,6 +45,8 @@ export default class First extends Component{
           }}
            source={require('../../logo.png')}/>
           <StatusBar backgroundColor='brown' barStyle="light-content"/>
+          
+          {this.state.sign ?
           <View style={styles.first}>
             <Button onPress={()=>Actions.signin()} style={styles.fbutton}>
                 <Text style={styles.btext}>Sign In </Text>
@@ -31,8 +55,25 @@ export default class First extends Component{
             <Button onPress={()=>Actions.signup()} style={styles.fbutton}>
                 <Text style={styles.btext}>Sign Up </Text>
             </Button>
-          </View>  
+          </View>
+          :
+          <View style={styles.first}>
+            <Spinner color="brown"/>
+          </View>
+          }
        </View>
     );
   }
 }
+const mapStateToProps = (state)=>{
+  return{}
+};
+const mapDispatchToProps = (dispatch)=>{
+  return { user: (user)=>
+    dispatch({type: "SIGN_UP",
+      user: user,
+      data: true
+    })
+  }
+  };
+export default connect(mapStateToProps, mapDispatchToProps)(First);
